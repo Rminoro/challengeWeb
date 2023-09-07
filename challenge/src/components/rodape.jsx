a9334d0c6a34cd79a565ef04fa8bd3203d5387a3
Rminoro <rminoro@outlook.com>
2023-09-07 16:38:02 -0300

    rodape

--- challenge/src/components/rodape.jsx
+++ challenge/src/components/rodape.jsx
@@ -5,6 +5,7 @@ export default function Rodape(){
             <h1>Entre em contato conosco</h1>
             <p>Quer saber mais sobre os Devs por trás do projeto? Clique no botão abaixo e confira</p>
             <button onClick="# ='#' ">Sobre nós</button>
+            <p>@Grupo ntc<span>|</span>grupontc1@gmail.com<span>|</span> (11)98716-6289</p>
         </footer>
        
         
