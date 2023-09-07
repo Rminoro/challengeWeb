2a497582368edf534719b10837a3645951912489
Rminoro <rminoro@outlook.com>
2023-09-07 16:37:56 -0300

    Conteudo rodape

--- challenge/src/routes/solicitar/index.jsx
+++ challenge/src/routes/solicitar/index.jsx
@@ -3,7 +3,12 @@ export default function Solicitar(){
     
     return(
         <main>
-            <h1> solicitar modal</h1>
+            <h1> Realizando o chmado do modal</h1>
+            <div><p> Através do HaarCascade, uma ferramenta da biblioteca OpenCV, poderemos  selecionar um pequeno número 
+                de características visuais críticas de uma determinada imagem e as utilizar para a etapa de detecção e 
+                classificação de objetos. Através de um “Scanner” nosso cliente poderá enviar
+                 sua localização e informações relevantes do veículo para que o chamado seja feito adequadamente.</p></div>
+                 
         </main>
     )
 }
